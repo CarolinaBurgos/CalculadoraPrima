415a73900e168828444a051f6b2cb5eff05979e3
CarolinaBurgos <caralbur@espol.edu.ec>
2020-01-21 19:29:59 -0500

    Trabajar en la solución del incidente

--- src/main/java/CalculadoraPrima/Prima.java
+++ src/main/java/CalculadoraPrima/Prima.java
@@ -2,10 +2,23 @@ package CalculadoraPrima;
 
 public class Prima {
 
-	 private int edad;
-	 private int puntos;
-	 private int tarifaBasica;
-	 private int maxPuntosLicencia;
+	 private transient int edad;
+	 private transient int puntos;
+	 private transient int tarifaBasica;
+	 private transient int maxPuntosLicencia;
+	 
+	 public static final int EDAD25 = 25;
+	 public static final int EDAD35 = 35;
+	 public static final int EDAD45 = 45;
+	 public static final int EDAD65 = 65;
+	 
+	 public static final int PUNTO1 = 1;
+	 public static final int PUNTO3 = 3;
+	 public static final int PUNTO5 = 5;
+	 public static final int PUNTO7 = 7;
+	 
+	 //Issue
+	 public static final int PUNTO4 = 4;
 	 
 	public Prima(int edad,int puntos) {
 		super();
@@ -21,37 +34,37 @@ public class Prima {
 		int ReducciónConductorSeguro=0;
 		int puntosPerdidos=maxPuntosLicencia-this.puntos;
 
-		if(this.edad<25) {
+		if(this.edad<EDAD25) {
 			factorEdad=2.8;
-			if(puntosPerdidos<=1){
+			if(puntosPerdidos<=PUNTO1){
 				ReducciónConductorSeguro=50;
 			}
 		}
 
-		if(this.edad>=25 && this.edad<35) {
+		if(this.edad>=EDAD25 && this.edad<EDAD35) {
 			factorEdad=1.8;
-			if(puntosPerdidos<=3){
+			if(puntosPerdidos<=PUNTO3){
 				ReducciónConductorSeguro=50;
 			}
 		}
 
-		if(this.edad>=35 && this.edad<45) {
+		if(this.edad>=EDAD35 && this.edad<EDAD45) {
 			factorEdad=1.0;
-			if(puntosPerdidos<=5){
+			if(puntosPerdidos<=PUNTO5){
 				ReducciónConductorSeguro=100;
 			}
 		}
 
-		if(this.edad>=45 && this.edad<65) {
+		if(this.edad>=EDAD45 && this.edad<EDAD65) {
 			factorEdad=0.8;
-			if(puntosPerdidos<=7){
+			if(puntosPerdidos<PUNTO7){
 				ReducciónConductorSeguro=150;
 			}
 		}
 
-		if(this.edad>=65) {
+		if(this.edad>=EDAD65) {
 			factorEdad=1.5;
-			if(puntosPerdidos<=5){
+			if(puntosPerdidos<=PUNTO4){
 				ReducciónConductorSeguro=200;
 			}
 		}
